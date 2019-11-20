175e0faba926f0bfb2e748f037d5dd5cb38988ec
Ray Zyphs <bruferviveiros@gmail.com>
2019-11-20 13:59:41 -0200

    Adicionado redirecionamento para a pagina correta ao click da receita

--- Modulo03/public/script.js
+++ Modulo03/public/script.js
@@ -1,5 +1,8 @@
 const cards = document.querySelectorAll(".recipe-card .image");
 
 for (let card of cards) {
-  card.addEventListener("click", () => console.log("x"));
+  card.addEventListener("click", e => {
+    const recipeIndex = e.path[1].getAttribute("data-index");
+    window.location.href = `/recipe/${recipeIndex}`;
+  });
 }
