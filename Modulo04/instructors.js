ae637c125acdabbc412ab2cf95d6b1b9714f0e38
Ray Zyphs <bruferviveiros@gmail.com>
2019-11-22 09:31:03 -0200

    Salvando dados de instrutores em um JSON

--- Modulo04/instructors.js
+++ Modulo04/instructors.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const data = require("./data.json");
 //CREATE
 exports.post = function(req, res) {
   const keys = Object.keys(req.body);
@@ -6,8 +8,18 @@ exports.post = function(req, res) {
     if (req.body[key] == "") return res.send("Please, fill all fields !");
   }
 
-  return res.send(req.body);
+  req.body.birth = Date.parse(req.body.birth);
+  req.body.created_at = Date.now();
+
+  data.instructors.push(req.body);
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Write file error!");
+
+    return res.redirect("/instructors");
+  });
 };
+
 //UPDATE
 
 //DELETE
