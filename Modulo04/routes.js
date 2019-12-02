fff6fea1877e102ad1a8efc7956cba8c8f2e35c8
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-02 11:58:06 -0200

    Criado pasta main para o site principal

--- Modulo04/routes.js
+++ Modulo04/routes.js
@@ -4,16 +4,18 @@ const recipes = require("./data");
 const routes = express.Router();
 
 //MAIN
-routes.get("/", (req, res) => res.render("home", { recipes }));
+routes.get("/", (req, res) => res.render("main/home", { recipes }));
 
-routes.get("/about", (req, res) => res.render("about"));
+routes.get("/about", (req, res) => res.render("main/about"));
 
-routes.get("/recipes", (req, res) => res.render("recipes", { recipes }));
+routes.get("/recipes", (req, res) => res.render("main/recipes", { recipes }));
 
 routes.get("/recipe/:index", (req, res) => {
   const recipeIndex = req.params.index;
 
-  return res.render("recipe", { recipe: recipes[recipeIndex] });
+  return res.render("main/recipe", { recipe: recipes[recipeIndex] });
 });
+
+//ADMIN
 
 module.exports = routes;
