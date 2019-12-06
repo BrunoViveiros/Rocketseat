4b9d2765c158d896e851339092b62903496e969b
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-06 10:56:22 -0200

    Pegando dados do create

---
+++ Modulo04/recipes.js
@@ -0,0 +1,8 @@
+exports.post = (req, res) => {
+  const keys = Object.keys(req.body);
+
+  for (key of keys)
+    if (req.body[key] == "") return res.send("Please, fill all fields");
+
+  return res.send(req.body);
+};
--- Modulo04/routes.js
+++ Modulo04/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const recipes = require("./data");
-
+const recipes = require("./recipes");
 const routes = express.Router();
 
 //MAIN
@@ -24,7 +23,7 @@ routes.get("/admin/recipes/create", (req, res) => res.render("admin/create")); /
 // routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
 // routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
-routes.post("/admin/recipes", (req, res) => res.send(req.body)); // Cadastrar nova receita
+routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
 // routes.put("/admin/recipes", recipes.put); // Editar uma receita
 // routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
