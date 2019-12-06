47298685f9eb4a4673b46e74a3147111575d1b72
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-06 12:56:29 -0200

    Feito tratamento de dados recebidos do form create

--- Modulo04/admin.js
+++ Modulo04/admin.js
@@ -1,8 +1,42 @@
+const fs = require("fs");
+const data = require("./data.json");
+
 exports.post = (req, res) => {
   const keys = Object.keys(req.body);
+  let {
+    image,
+    title,
+    author,
+    ingredients,
+    preparation,
+    information
+  } = req.body;
 
   for (key of keys)
     if (req.body[key] == "") return res.send("Please, fill all fields");
 
-  return res.send(req.body);
+  if (typeof ingredients === "string") {
+    ingredients = Array(ingredients);
+  }
+  if (typeof preparation === "string") {
+    preparation = Array(preparation);
+  }
+
+  const id = Number(data.recipes.length + 1);
+
+  data.recipes.push({
+    id,
+    image,
+    title,
+    author,
+    ingredients,
+    preparation,
+    information
+  });
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
+    if (err) return res.send("Write file error!");
+
+    return res.redirect("/admin/recipes");
+  });
 };
