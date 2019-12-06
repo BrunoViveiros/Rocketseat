3ef85ebf1a6f757f68d2a582d4a8ee22f586884e
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-06 10:34:27 -0200

    Adicionado method post no formulario create

--- Modulo04/routes.js
+++ Modulo04/routes.js
@@ -24,7 +24,7 @@ routes.get("/admin/recipes/create", (req, res) => res.render("admin/create")); /
 // routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
 // routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
-// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+routes.post("/admin/recipes", (req, res) => res.send("recebido")); // Cadastrar nova receita
 // routes.put("/admin/recipes", recipes.put); // Editar uma receita
 // routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
