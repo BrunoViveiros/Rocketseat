59d77e19dcb8a709f9e63f1ab941d17b975cfa57
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-11 15:16:24 -0200

    Feito o Algoritmo de Paginação para o Front

--- Modulo05/public/scripts.js
+++ Modulo05/public/scripts.js
@@ -6,3 +6,29 @@ for (item of menuItems) {
     item.classList.add("active");
   }
 }
+
+//Paginação
+let totalPages = 20,
+  selectedPage = 15,
+  pages = [],
+  oldPage;
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
+    pages.push(currentPage);
+
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push("...");
+    }
+
+    if (oldPage && currentPage - oldPage == 2) {
+      pages.push(oldPage + 1);
+    }
+
+    oldPage = currentPage;
+  }
+}
