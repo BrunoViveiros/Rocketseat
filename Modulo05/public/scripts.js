2c84bec1035c8c7f9fa7b44e8abe0350225c6024
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-11 15:40:58 -0200

    Adicionado ao algoritimo de paginação na parte do front

--- Modulo05/public/scripts.js
+++ Modulo05/public/scripts.js
@@ -8,27 +8,37 @@ for (item of menuItems) {
 }
 
 //Paginação
-let totalPages = 20,
-  selectedPage = 15,
-  pages = [],
-  oldPage;
 
-for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
-  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
-  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
-  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+function paginate(selectedPage, totalPages) {
+  let pages = [],
+    oldPage;
 
-  if (firstAndLastPage || (pagesBeforeSelectedPage && pagesAfterSelectedPage)) {
-    pages.push(currentPage);
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
 
-    if (oldPage && currentPage - oldPage > 2) {
-      pages.push("...");
+    if (
+      firstAndLastPage ||
+      (pagesBeforeSelectedPage && pagesAfterSelectedPage)
+    ) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...");
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1);
+      }
+
+      oldPage = currentPage;
+
+      pages.push(currentPage);
     }
-
-    if (oldPage && currentPage - oldPage == 2) {
-      pages.push(oldPage + 1);
-    }
-
-    oldPage = currentPage;
   }
+  return pages;
 }
+
+const pagination = document.querySelector(".pagination");
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const pages = paginate(page, total);
