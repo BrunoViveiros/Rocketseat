eaf2568319ca1df5469d21271e64ebec8cb68e40
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-16 15:24:00 -0300

    Arrumando db

--- Modulo05/src/config/db.js
+++ Modulo05/src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   user: "rayzyphs",
-  password: "Bruno-3720",
+  password: "bruno",
   host: "localhost",
   port: 5432,
   database: "gymmanager"
