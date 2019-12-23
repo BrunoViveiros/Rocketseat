ff025f2a003dd7c2b7472ef92ca1147a9882846d
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-23 20:10:28 -0200

    Finalizando Gerenciador de Imagens de Produtos

--- Modulo06/public/scripts.js
+++ Modulo06/public/scripts.js
@@ -15,14 +15,19 @@ const Mask = {
 };
 
 const PhotosUpload = {
+  input: "",
   preview: document.querySelector("#photos-preview"),
   uploadLimit: 6,
+  files: [],
   handleFileInput(event) {
     const { files: fileList } = event.target;
+    PhotosUpload.input = event.target;
 
     if (PhotosUpload.hasLimit(event)) return;
 
     Array.from(fileList).forEach(file => {
+      PhotosUpload.files.push(file);
+
       const reader = new FileReader();
 
       reader.onload = () => {
@@ -35,18 +40,41 @@ const PhotosUpload = {
 
       reader.readAsDataURL(file);
     });
+
+    PhotosUpload.input.files = PhotosUpload.getAllfiles();
   },
   hasLimit(event) {
-    const { uploadLimit } = PhotosUpload;
-    const { files: fileList } = event.target;
-
+    const { uploadLimit, input, preview } = PhotosUpload;
+    const { files: fileList } = input;
+    
     if (fileList.length > uploadLimit) {
       alert(`Envie no maximo ${uploadLimit} fotos`);
       event.preventDefault();
       return true;
     }
 
+    const photosDiv = [];
+    preview.childNodes.forEach(item => {
+      if (item.classList && item.classList.value == "photo")
+        photosDiv.push(item);
+    });
+
+    const totalPhotos = fileList.length + photosDiv.length;
+    if (totalPhotos > uploadLimit) {
+      alert("Você atingiu o limite máximo de fotos");
+      event.preventDefault();
+      return true;
+    }
+
     return false;
+  },
+  getAllfiles() {
+    const dataTransfer =
+      new ClipboardEvent("").clipboardData || new DataTransfer();
+
+    PhotosUpload.files.forEach(file => dataTransfer.items.add(file));
+
+    return dataTransfer.files;
   },
   getContainer(image) {
     const div = document.createElement("div");
@@ -70,6 +98,9 @@ const PhotosUpload = {
     const photoDiv = event.target.parentNode;
     const photosArray = Array.from(PhotosUpload.preview.children);
     const index = photosArray.indexOf(photoDiv);
+
+    PhotosUpload.files.splice(index, 1);
+    PhotosUpload.input.files = PhotosUpload.getAllfiles();
 
     photoDiv.remove();
   }
