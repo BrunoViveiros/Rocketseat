0b30f994c0346ee49b2f9fc8ff61e7becaff6d69
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-23 18:51:12 -0200

    Feito atualização e exclusão de produtos

--- Modulo06/src/app/controllers/ProductController.js
+++ Modulo06/src/app/controllers/ProductController.js
@@ -31,14 +31,6 @@ module.exports = {
     return res.redirect(`products/${productId}`);
   },
   async edit(req, res) {
-    const keys = Object.keys(req.body);
-
-    for (key of keys) {
-      if (req.body[key] == "") {
-        return res.send("Please, fill all fields!");
-      }
-    }
-
     let results = await Product.find(req.params.id);
     const product = results.rows[0];
 
@@ -51,5 +43,30 @@ module.exports = {
     const categories = results.rows;
 
     return res.render("products/edit.njk", { product, categories });
+  },
+  async put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please, fill all fields!");
+      }
+    }
+
+    req.body.price = req.body.price.replace(/\D/g, "");
+
+    if (req.body.old_price != req.body.price) {
+      const oldProduct = await Product.find(req.body.id);
+      req.body.old_price = oldProduct.rows[0].price;
+    }
+
+    await Product.update(req.body);
+
+    return res.redirect(`/products/${req.body.id}/edit`);
+  },
+  async delete(req, res) {
+    await Product.delete(req.body.id);
+
+    return res.redirect("/products/create");
   }
 };
