c892955b65b917c745ca40834b11751cdda5f06d
Ray Zyphs <bruferviveiros@gmail.com>
2019-12-27 11:41:45 -0200

    Feito adição de imagens no gerenciador na parte de edição

--- Modulo06/src/app/controllers/ProductController.js
+++ Modulo06/src/app/controllers/ProductController.js
@@ -36,7 +36,7 @@ module.exports = {
     const filesPromise = req.files.map(file =>
       File.create({ ...file, product_id: productId })
     );
-    await Promise.all(filesPromise)
+    await Promise.all(filesPromise);
 
     return res.redirect(`products/${productId}/edit`);
   },
@@ -49,10 +49,19 @@ module.exports = {
     product.old_price = formatPrice(product.old_price);
     product.price = formatPrice(product.price);
 
+    //get categories
     results = await Category.all();
     const categories = results.rows;
 
-    return res.render("products/edit.njk", { product, categories });
+    //get images
+    results = await Product.files(product.id);
+    let files = results.rows;
+    files = files.map(file => ({
+      ...files,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+    }));
+
+    return res.render("products/edit.njk", { product, categories, files });
   },
   async put(req, res) {
     const keys = Object.keys(req.body);
