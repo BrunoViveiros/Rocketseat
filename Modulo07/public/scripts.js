5bd01cc33f35acbfb753b4b5b3cbea8563c8f9d9
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-02 17:08:41 -0200

    Feito Mascara de CPF, CNPJ e CEP

--- Modulo07/public/scripts.js
+++ Modulo07/public/scripts.js
@@ -11,6 +11,55 @@ const Mask = {
       style: "currency",
       currency: "BRL"
     }).format(value / 100));
+  },
+  cpfCnpj(value) {
+    value = value.replace(/\D/g, "");
+
+    if (value.length > 14) {
+      value = value.slice(0, -1);
+    }
+
+    // check if is cnpj - 11.222.333/0001-11
+    if (value.length > 11) {
+      //11222333000111
+
+      //11.222333000111
+      value = value.replace(/(\d{2})(\d)/, "$1.$2");
+
+      //11.222.333000111
+      value = value.replace(/(\d{3})(\d)/, "$1.$2");
+
+      //11.222.333/000111
+      value = value.replace(/(\d{3})(\d)/, "$1/$2");
+
+      //11.222.333/000111
+      value = value.replace(/(\d{4})(\d)/, "$1-$2");
+    } else {
+      //cpf 111.222.333-44
+      //11122233344
+
+      //111.22233344
+      value = value.replace(/(\d{3})(\d)/, "$1.$2");
+
+      //111.222.33344
+      value = value.replace(/(\d{3})(\d)/, "$1.$2");
+
+      //111.222.333-44
+      value = value.replace(/(\d{3})(\d)/, "$1-$2");
+    }
+
+    return value;
+  },
+  cep(value) {
+    value = value.replace(/\D/g, "");
+
+    if (value.length > 8) {
+      value = value.slice(0, -1);
+    }
+
+    value = value.replace(/(\d{5})(\d)/, "$1-$2");
+
+    return value;
   }
 };
 
