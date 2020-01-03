4d1087fdc190cd26c409b6174fb36348bd48aa6d
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-03 14:57:33 -0200

    Alteração na tabela da db para reset tokens

--- Modulo07/src/app/controllers/SessionController.js
+++ Modulo07/src/app/controllers/SessionController.js
@@ -14,5 +14,13 @@ module.exports = {
   forgotForm(req, res) {
     return res.render("session/forgot-password");
   },
-  forgot(req, res) {}
+  forgot(req, res) {
+    //um token para esse usuario
+
+    //criar uma expiração para o token
+
+    //enviar um email com o um link de recuperação de senha
+
+    //avisar o usuario que enviamos o email
+  }
 };
