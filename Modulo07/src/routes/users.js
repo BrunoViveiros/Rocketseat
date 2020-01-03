8db53b4ffb681af88fcb9acb067ec33c0c1a3207
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-03 16:39:54 -0200

    SQL Cascade para Remover Usuários, Produtos e Imagens

--- Modulo07/src/routes/users.js
+++ Modulo07/src/routes/users.js
@@ -12,23 +12,23 @@ const {
   onlyUsers
 } = require("../app/middlewares/session");
 
-// // login / logout
+// login / logout
 routes.get("/login", isLoggedRedirectToUsers, SessionController.loginForm);
 routes.post("/login", SessionValidator.login, SessionController.login);
 routes.post("/logout", SessionController.logout);
 
-// // reset password / forgot
+// reset password / forgot
 routes.get("/forgot-password", SessionController.forgotForm);
 routes.get("/password-reset", SessionController.resetForm);
 routes.post("/forgot-password", SessionValidator.forgot, SessionController.forgot);
 routes.post("/password-reset", SessionValidator.reset, SessionController.reset);
 
-// // user register
+// user register
 routes.get("/register", UserController.registerForm);
 routes.post("/register", UserValidator.post, UserController.post);
 
 routes.get("/", onlyUsers, UserValidator.show, UserController.show);
 routes.put("/", UserValidator.update, UserController.update);
-// routes.delete("/", UserController.delete);
+routes.delete("/", UserController.delete);
 
 module.exports = routes;
