ae25a31fddb0bf97d76065700f6e214dec69bd63
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-03 16:15:35 -0200

    Validando a Recuperação de Senha

--- Modulo07/src/app/controllers/SessionController.js
+++ Modulo07/src/app/controllers/SessionController.js
@@ -60,5 +60,28 @@ module.exports = {
         error: "Erro inesperado, tente novamente!"
       });
     }
+  },
+  resetForm(req, res) {
+    return res.render("session/password-reset", { token: req.query.token });
+  },
+  reset(req, res) {
+    const { email, password, passwordRepeat, token } = req.body;
+
+    try {
+
+
+      //cria um novo hash de senha
+
+      //atualiza o usuario
+
+      //avisa o usuario que ele tem uma nova senha
+
+      
+    } catch (err) {
+      console.error(err);
+      return res.render("session/password-reset", {
+        error: "Erro inesperado, tente novamente!"
+      });
+    }
   }
 };
