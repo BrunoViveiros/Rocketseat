11a5f8b7e82a507b186f569dc257e932c47a93ed
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-03 09:27:20 -0200

    Feito Alertas e Mensagens do Back para o Front

--- Modulo07/src/app/validators/user.js
+++ Modulo07/src/app/validators/user.js
@@ -1,33 +1,44 @@
 const User = require("../models/User");
 
 async function post(req, res, next) {
-      //check if has all fields
-      const keys = Object.keys(req.body);
+  //check if has all fields
+  const keys = Object.keys(req.body);
 
-      for (key of keys) {
-        if (req.body[key] == "") {
-          return res.send("Please, fill all fields!");
-        }
-      }
-  
-      //check if user exists [email, cpf_cnpj]
-      let { email, cpf_cnpj, password, passwordRepeat } = req.body;
-  
-      cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
-  
-      const user = await User.findOne({
-        where: { email },
-        or: { cpf_cnpj }
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.render("user/register", {
+        user: req.body,
+        error: "Por favor, preencha todos os campos."
       });
-  
-      if (user) return res.send("Users exists");
-  
-      //check if password match
-      if (password !== passwordRepeat) return res.send("Password Mismatch");
+    }
+  }
 
-      next()
+  //check if user exists [email, cpf_cnpj]
+  let { email, cpf_cnpj, password, passwordRepeat } = req.body;
+
+  cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
+
+  const user = await User.findOne({
+    where: { email },
+    or: { cpf_cnpj }
+  });
+
+  if (user)
+    return res.render("user/register", {
+      user: req.body,
+      error: "Usuário já cadastrado."
+    });
+
+  //check if password match
+  if (password !== passwordRepeat)
+    return res.render("user/register", {
+      user: req.body,
+      error: "A senha e a repetição da senha estão incorretas."
+    });
+
+  next();
 }
 
 module.exports = {
   post
-}
+};
