ae942954192c25ca3faadf9ef426265e9a8369cf
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-04 15:20:52 -0200

    Utilizando o Base Model

--- Modulo08/src/app/models/Base.js
+++ Modulo08/src/app/models/Base.js
@@ -1,5 +1,20 @@
 const db = require("../../config/db");
 
+function find(filters, table) {
+  let query = `SELECT * FROM ${table}`;
+
+  Object.keys(filters).map(key => {
+    //WHERE | OR | AND
+    query += ` ${key}`;
+
+    Object.keys(filters[key]).map(field => {
+      query += ` ${field} = '${filters[key][field]}'`;
+    });
+  });
+
+  return db.query(query);
+}
+
 const Base = {
   init({ table }) {
     if (!table) throw new Error("Invalid Params");
@@ -8,24 +23,17 @@ const Base = {
 
     return this;
   },
-  async findOne(filters) {
-    let query = `SELECT * FROM ${this.table}`;
-
-    Object.keys(filters).map(key => {
-      //WHERE | OR | AND
-      query = `
-        ${query}
-        ${key}
-      `;
-
-      Object.keys(filters[key]).map(field => {
-        query = `${query} ${field} = '${filters[key][field]}'`;
-      });
-    });
-
-    const results = await db.query(query);
-
+  async find(id) {
+    const results = await find({ where: { id } }, this.table);
     return results.rows[0];
+  },
+  async findOne(filters) {
+    const results = await find(filters, this.table);
+    return results.rows[0];
+  },
+  async findAll(filters) {
+    const results = await find(filters, this.table);
+    return results.rows;
   },
   async create(fields) {
     try {
@@ -64,13 +72,12 @@ const Base = {
       `;
 
       return db.query(query);
-      
     } catch (error) {
       console.error(error);
     }
   },
   delete(id) {
-    return db.query("DELETE FROM products WHERE id = $1", [id]);
+    return db.query(`DELETE FROM ${this.table} WHERE id = $1`, [id]);
   }
 };
 
