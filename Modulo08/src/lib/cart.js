be75befc9f2fdb7f78ab2c527e430b78076fa19b
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-07 13:14:03 -0200

    Feito estrutura do carrinho de compras

---
+++ Modulo08/src/lib/cart.js
@@ -0,0 +1,26 @@
+const { formatPrice } = require("./utils");
+
+// carrinho fica guardado na sessão (req.session)
+
+const Cart = {
+  init(oldCart) {
+    if (oldCart) {
+      this.items = oldCart.items;
+      this.total = oldCart.total;
+    } else {
+      this.items = [];
+      this.total = {
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0)
+      };
+    }
+
+    return this;
+  },
+  addOne(product) {},
+  removeOne(productId) {},
+  delete(productId) {}
+};
+
+module.exports = Cart;
