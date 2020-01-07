b11747ab9cb589d22ec619fa7cdaff4b08ec5530
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-07 15:38:04 -0200

    Adicionando Itens ao Carrinho

--- Modulo08/src/lib/cart.js
+++ Modulo08/src/lib/cart.js
@@ -18,9 +18,48 @@ const Cart = {
 
     return this;
   },
-  addOne(product) {},
+  addOne(product) {
+    // ver se o produto já existe no carrinho
+    let inCart = this.items.find(item => item.product.id == product.id);
+
+    // se não existe
+    if (!inCart) {
+      inCart = {
+        product: {
+          ...product,
+          formattedPrice: formatPrice(product.price)
+        },
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0)
+      };
+
+      this.items.push(inCart);
+    }
+
+    // max quantity exceed
+    if (inCart.quantity >= product.quantity) return this;
+
+    // update item
+    inCart.quantity++;
+    inCart.price = inCart.product.price * inCart.quantity;
+    inCart.formattedPrice = formatPrice(inCart.price);
+
+    // update cart
+    this.total.quantity++;
+    this.total.price += inCart.product.price;
+    this.total.formattedPrice = formatPrice(this.total.price);
+
+    return this;
+  },
   removeOne(productId) {},
   delete(productId) {}
+};
+
+const product = {
+  id: 1,
+  prices: 199,
+  quantity: 2
 };
 
 module.exports = Cart;
