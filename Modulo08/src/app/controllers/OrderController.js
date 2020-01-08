893c9c396f5322255f217a7e7ded0aa5a53fc9a9
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-08 20:23:21 -0200

    Mudando status do pedido

--- Modulo08/src/app/controllers/OrderController.js
+++ Modulo08/src/app/controllers/OrderController.js
@@ -104,5 +104,39 @@ module.exports = {
       console.error(error);
       return res.render("orders/error");
     }
+  },
+  async update(req, res) {
+    try {
+      const { id, action } = req.params;
+      const acceptedActions = ["close", "cancel"];
+      if (!acceptedActions.includes(action))
+        return res.send("Can't do this action");
+
+      // pegar o pedido
+      const order = await Order.findOne({
+        where: { id }
+      });
+
+      if (!order) return res.send("Order not found");
+      // verificar se esta aberto
+      if (order.status != "open") return res.send("Can't do this action");
+
+      // atualizar o pedido
+      const statuses = {
+        close: "sold",
+        cancel: "cancelled"
+      };
+
+      order.status = statuses[action];
+
+      await Order.update(id, {
+        status: order.status
+      });
+
+      //redirecionar
+      return res.redirect("/orders/sales");
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
