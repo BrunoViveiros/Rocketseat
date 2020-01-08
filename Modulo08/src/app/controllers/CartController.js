b0a0d6a3ea73bc79888114452345e5de4de01bf4
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-08 10:07:31 -0200

    Utilizando a Funcionalidade de Adicionar ao Carrinho

--- Modulo08/src/app/controllers/CartController.js
+++ Modulo08/src/app/controllers/CartController.js
@@ -5,17 +5,30 @@ const LoadProductService = require("../services/LoadProductService");
 module.exports = {
   async index(req, res) {
     try {
-
-      const product = await LoadProductService.load('product', {where: {id: 23}})
-
       let { cart } = req.session;
 
       // gerenciador de carrinho
-      cart = Cart.init(cart).addOne(product);
+      cart = Cart.init(cart)
 
       return res.render("cart/index", { cart });
     } catch (error) {
       console.error(error);
     }
+  },
+  async addOne(req, res) {
+    //pegar o id do produto e o produto
+    const { id } = req.params;
+
+    const product = await LoadProductService.load("product", {
+      where: { id }
+    });
+    //pegar o carrinho da sessao
+    let { cart } = req.session;
+    //adicionar o produto ao carrinho
+    cart = Cart.init(cart).addOne(product);
+    //atualizar o carrinho da sessao
+    req.session.cart = cart;
+    //redirecionar o usuario para a tela do carrinho
+    return res.redirect("/cart");
   }
 };
--- Modulo08/src/routes/cart.js
+++ Modulo08/src/routes/cart.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 
 const CartController = require("../app/controllers/CartController");
 
-routes.get("/", CartController.index);
+routes.get("/", CartController.index)
+      .post('/:id/add-one', CartController.addOne)
 
+
+
+      
 module.exports = routes;
