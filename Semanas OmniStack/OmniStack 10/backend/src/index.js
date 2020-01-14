245533955a761d28512b11ae427cf1c23b5d4f69
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-14 19:00:28 -0200

    rota de criação de dev

---
+++ Semanas OmniStack/OmniStack 10/backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack-1v5m7.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
