27e94a2317f28e57703ec4d8135302ec35739e6b
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-16 16:32:47 -0200

    Feito parte de visualização do app

--- Semanas OmniStack/OmniStack 10/mobile/src/pages/Main.js
+++ Semanas OmniStack/OmniStack 10/mobile/src/pages/Main.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Image, View, Text } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 import MapView, { Marker, Callout } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
 } from "expo-location";
+import { MaterialIcons } from "@expo/vector-icons";
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -37,27 +45,47 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -20.5398086, longitude: -47.4183587 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: "https://avatars0.githubusercontent.com/u/27422266?s=460&v=4"
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -20.5398086, longitude: -47.4183587 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://avatars0.githubusercontent.com/u/27422266?s=460&v=4"
+            }}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "BrunoViveiros"
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Bruno Fernandes</Text>
+              <Text style={styles.devBio}>
+                eu sou uma biografio ldasld lçasd as asd as
+              </Text>
+              <Text style={styles.devTechs}>ReactJS, NodeJS, React Native</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
         />
-        <Callout onPress={() => {
-          navigation.navigate('Profile', { github_username: 'BrunoViveiros' });
-        }}>
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Bruno Fernandes</Text>
-            <Text style={styles.devBio}>
-              eu sou uma biografio ldasld lçasd as asd as
-            </Text>
-            <Text style={styles.devTechs}>ReactJS, NodeJS, React Native</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
@@ -76,7 +104,7 @@ const styles = StyleSheet.create({
     width: 260
   },
   devName: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 16
   },
   devBio: {
@@ -85,6 +113,39 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5
+  },
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8e4dff",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
   }
 });
 
