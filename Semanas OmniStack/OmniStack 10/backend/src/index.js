c908a30a81e43da1146ea18f8601013cc709c368
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-17 15:11:09 -0200

    Adicionado websockets e feito atualização de devs em tempo real

--- Semanas OmniStack/OmniStack 10/backend/src/index.js
+++ Semanas OmniStack/OmniStack 10/backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 const routes = require("./routes");
+const { setupWebSocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@omnistack-1v5m7.mongodb.net/week10?retryWrites=true&w=majority",
@@ -19,4 +24,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
